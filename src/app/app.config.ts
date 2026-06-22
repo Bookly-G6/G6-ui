cbf5dc8576490358f54e584a78bfba3f8017739a
Ailin Ayelen Miño <ailinayelenmino@gmail.com>
2026-06-22 05:22:09 -0300

    feat: agregar componente de carga global y optimizar la gestión de solicitudes HTTP

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { authTokenInterceptor } from './core/interceptors/auth-token.interceptor';
+import { loadingInterceptor } from './core/interceptors/loading.interceptor';
 
 import { routes } from './app.routes';
 
@@ -10,7 +11,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authTokenInterceptor])),
+    provideHttpClient(withInterceptors([loadingInterceptor, authTokenInterceptor])),
     provideAnimations(),
   ],
 };
--- src/app/app.ts
+++ src/app/app.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/components/toast/toast.component';
 import { AuthSessionService } from './core/services/auth-session.service';
+import { GlobalLoadingComponent } from './shared/components/global-loading/global-loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent, GlobalLoadingComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
